666a573d6d9f9fcf3419326bb417a0e1d37b2fb8
dorsapiri <dorsapiri@yahoo.com>
2014-11-30 17:00:51 +0330

    pass the username after login

---
+++ App_Code/UserLogon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+/// <summary>
+/// Summary description for UserLogon
+/// </summary>
+public class UserLogon
+{
+    public UserLogon()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
